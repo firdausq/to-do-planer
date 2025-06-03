976da8b9d25e33791dc837740bb65fcf07786206
U.M <firdaus.q@protonmail.com>
2025-06-03 18:11:01 +0200

    Fixed General Issues

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,6 +6,7 @@ import WeatherWidget from '../features/weather/WeatherWidget'
 import CalendarView  from '../features/calendar/CalendarView'
 import TodosPanel    from '../features/todos/TodosPanel'
 import NotesPanel    from '../features/notes/NotesPanel'
+import styles from '../components/Layout.module.css'
 
 export default function MainPage() {
   return (
@@ -14,8 +15,12 @@ export default function MainPage() {
       <div style={{ gridArea: 'weather'  }}><WeatherWidget /></div>
       <div style={{ gridArea: 'note'     }}><DailyNote /></div>
       <div style={{ gridArea: 'calendar' }}><CalendarView /></div>
-      <div style={{ gridArea: 'todos'    }}><TodosPanel /></div>
-      <div style={{ gridArea: 'notes'    }}><NotesPanel /></div>
+      <div style={{ gridArea: 'rightColumn' }}>
+        <div className={styles.rightStack}>
+          <TodosPanel />
+          <NotesPanel />
+        </div>
+      </div>
     </Layout>
   )
 }
